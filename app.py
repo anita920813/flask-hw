28257585196cceb186612fa270d1099fd60f27f1
anita920813 <122900169+anita920813@users.noreply.github.com>
2026-06-05 11:28:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # 這是老師簡報最後一頁教的：讓 Python 去控制並回傳 templates 資料夾底下的 index.html
-    return "Hello World! This is my first Flask app."
+    # 對照簡報：建立一個 Python 變數，並透過 render_template 傳給前端
+    username = "謝宜蓁"
+    return render_template("index.html", name=username)
 
 if __name__ == "__main__":
     app.run(debug=True)
